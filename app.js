5fe5889ee2051e8ab359d083edd39a1aa186c08e
Tom Hames <40021462+TomHames@users.noreply.github.com>
2021-08-22 15:31:01 +0100

    Implemented server logic

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+// mock database
+let db = [];
 
-app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
+let streamCount = 0;
+
+app.get("/", (req, res) => res.send("Hello, World!"));
+
+app.post("/user/:userId/stream/:streamId", (req, res) => {
+  const { userId, streamId } = req.params;
+
+  // check if stream exists in database
+  if (db.some((el) => el.streamId === streamId)) {
+    console.log(`${streamId} already exists in database.`);
+
+    res.statusCode = 200;
+    res.send(`${streamId} already exists. Try another one! :D`);
+  }
+  // check if streams over 3
+  else if (streamCount > 2) {
+    console.log(`Maximum number of streams reached.`);
+
+    res.statusCode = 403;
+    res.send(
+      `You've reached your maximum number of streams to watch at the same time. Oops!`
+    );
+  }
+  // add stream to database
+  else {
+    db.push({ userId, streamId });
+    streamCount++;
+    console.log(`Added ${streamId} to database.`);
+
+    res.statusCode = 201;
+    res.send("Here is your stream. Enjoy!");
+  }
+});
+
+app.listen(port, console.log(`Server listening on http://localhost:${port}`));
